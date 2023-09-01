883277ffa54fc76fc7c04bd95c02fb3f23d42d00
chinmayHefty <chinmay@heftyart.com>
2023-09-01 21:30:12 +0530

    init express and installed firebase admin sdk

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-function main() {
-  console.log(" new data");
-}
-main();
+import express from "express";
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+app.use("/request", (req, res, next) => {
+  console.log("Request type: ", req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log("GET home route");
+  res.send("ok").status(200);
+});
+
+app.get("/request", (req, res) => {
+  console.log("GET request route");
+  res.send("ok").status(200);
+});
+
+app.listen("3001", () => {
+  console.log("listening on http://localhost:3001");
+});
