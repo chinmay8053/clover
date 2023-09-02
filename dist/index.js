d2050e724d0d6c9f9b8bdf70d0c81039401bb23b
chinmayHefty <chinmay@heftyart.com>
2023-09-02 15:31:51 +0530

    build

--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(body_parser_1.default);
 app.use((req, res, next) => {
     console.log("Time: ", Date.now());
     next();
 });
 app.use("/request", (req, res, next) => {
-    console.log("Request type: ", req.method);
+    console.log("Request type: ", req);
     next();
 });
 app.get("/", (req, res) => {
